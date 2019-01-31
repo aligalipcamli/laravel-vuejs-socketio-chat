c5faa12a948b8bbdf2acc605e090e5fe834abd44
Ali Galip Çamlı <aligalip@camli.info>
2019-01-31 16:33:53 +0300

    ExampleEvent & broadcasting in action

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -54,3 +54,20 @@ if (token) {
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+import Echo from "laravel-echo"
+window.io = require('socket.io-client');
+
+// Have this in case you stop running your laravel echo server
+
+if (typeof io !== 'undefined') {
+  window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001',
+  });
+}
+
+window.Echo.channel('test-event')
+    .listen('ExampleEvent', (e) => {
+        console.log(e);
+    });
