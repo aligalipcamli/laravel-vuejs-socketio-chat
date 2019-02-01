5941e2132fa2514ae0cd1f2d4cb03897fd530c7f
Ali Galip Çamlı <aligalip@camli.info>
2019-02-01 17:38:16 +0300

    Register the components & client-side methods

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,12 +22,39 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+Vue.component('chat-messages', require('./components/ChatMessages.vue').default);
+Vue.component('chat-form', require('./components/ChatForm.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+ const app = new Vue({
+     el: '#app',
+
+     data: {
+         messages: []
+     },
+
+     created() {
+         this.fetchMessages();
+     },
+
+     methods: {
+         fetchMessages() {
+             axios.get('/messages').then(response => {
+                 this.messages = response.data;
+             });
+         },
+
+         addMessage(message) {
+             this.messages.push(message);
+
+             axios.post('/messages', message).then(response => {
+               console.log(response.data);
+             });
+         }
+     }
+ });
